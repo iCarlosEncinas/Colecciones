044ada20ee85bc78aff746b5fefb551f584d5522
Carlos Encinas <carlos-encinas@hotmail.com>
2019-08-22 12:21:46 -0700

    Agregar archivos de proyecto.

---
+++ Colecciones/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alumno> alumnos = new List<Alumno>();
+            alumnos.Add(new Alumno("Roberto" , "189222"));
+            alumnos.Add(new Alumno("Ana", "189223"));
+            alumnos.Add(new Alumno("Karina", "189224"));
+            alumnos.Add(new Alumno("Juan", "189225"));
+            alumnos.Add(new Alumno("Nicolás", "189226"));
+
+            foreach(Alumno alumno in alumnos)
+            {
+                Console.WriteLine("Nombre:     " + alumno.Nombre + "\nMatrícula:  " + alumno.Matricula + "\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
